869b486c3ba92b983170ba653788721c5edfd4ad
F-K-C <fakocarvalho@gmail.com>
2024-11-04 23:50:55 +0000

    feat(home): add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,161 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="bg-red pb-6 sm:pb-8 lg:pb-12">
+  <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+    <header className="mb-4 flex items-center justify-between py-4 md:py-8">
+      { /* logo - start */ }
+      <a href="/" className="inline-flex items-center gap-2.5 text-2xl font-bold text-black md:text-3xl" aria-label="logo">
+        <svg width="95" height="94" viewBox="0 0 95 94" className="h-auto w-6 text-indigo-500" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path d="M96 0V47L48 94H0V47L48 0H96Z" />
+        </svg>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        Flowrift
+      </a>
+      { /* logo - end */ }
+
+      { /* nav - start */ }
+      <nav className="hidden gap-12 lg:flex">
+        <a href="#" className="text-lg font-semibold text-indigo-500">Home</a>
+        <a href="#" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">Features</a>
+        <a href="#" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">Pricing</a>
+        <a href="#" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">About</a>
+      </nav>
+      { /* nav - end */ }
+
+      { /* buttons - start */ }
+      <a href="#" className="hidden rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:inline-block">Contact Sales</a>
+
+      <button type="button" className="inline-flex items-center gap-2 rounded-lg bg-gray-200 px-2.5 py-2 text-sm font-semibold text-gray-500 ring-indigo-300 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
+        </svg>
+
+        Menu
+      </button>
+      { /* buttons - end */ }
+    </header>
+
+    <section className="min-h-96 relative flex flex-1 shrink-0 items-center justify-center overflow-hidden rounded-lg bg-gray-100 py-16 shadow-lg md:py-20 xl:py-48">
+      { /* image - start */ }
+      <img src="https://images.unsplash.com/photo-1618004652321-13a63e576b80?auto=format&q=75&fit=crop&w=1500" loading="lazy" alt="Photo by Fakurian Design" className="absolute inset-0 h-full w-full object-cover object-center" />
+      { /* image - end */ }
+
+      { /* overlay - start */ }
+      <div className="absolute inset-0 bg-indigo-500 mix-blend-multiply"></div>
+      { /* overlay - end */ }
+
+      { /* text start */ }
+      <div className="relative flex flex-col items-center p-4 sm:max-w-xl">
+        <p className="mb-4 text-center text-lg text-indigo-200 sm:text-xl md:mb-8">Very proud to introduce</p>
+        <h1 className="mb-8 text-center text-4xl font-bold text-white sm:text-5xl md:mb-12 md:text-6xl">Revolutionary way to build the web</h1>
+
+        <div className="flex w-full flex-col gap-2.5 sm:flex-row sm:justify-center">
+          <a href="#" className="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base">Start now</a>
+
+          <a href="#" className="inline-block rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base">Take tour</a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      </div>
+      { /* text end */ }
+    </section>
+  </div>
+</div>
+  )
+  // return (
+  //   <div classNameName="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+  //     <main classNameName="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+  //       <Image
+  //         classNameName="dark:invert"
+  //         src="/next.svg"
+  //         alt="Next.js logo"
+  //         width={180}
+  //         height={38}
+  //         priority
+  //       />
+  //       <ol classNameName="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+  //         <li classNameName="mb-2">
+  //           Get started by editing{" "}
+  //           <code classNameName="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+  //             src/app/page.tsx
+  //           </code>
+  //           .
+  //         </li>
+  //         <li>Save and see your changes instantly.</li>
+  //       </ol>
+
+  //       <div classNameName="flex gap-4 items-center flex-col sm:flex-row">
+  //         <a
+  //           classNameName="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+  //           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //         >
+  //           <Image
+  //             classNameName="dark:invert"
+  //             src="/vercel.svg"
+  //             alt="Vercel logomark"
+  //             width={20}
+  //             height={20}
+  //           />
+  //           Deploy now
+  //         </a>
+  //         <a
+  //           classNameName="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+  //           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //         >
+  //           Read our docs
+  //         </a>
+  //       </div>
+  //     </main>
+  //     <footer classNameName="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+  //       <a
+  //         classNameName="flex items-center gap-2 hover:underline hover:underline-offset-4"
+  //         href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         <Image
+  //           aria-hidden
+  //           src="/file.svg"
+  //           alt="File icon"
+  //           width={16}
+  //           height={16}
+  //         />
+  //         Learn
+  //       </a>
+  //       <a
+  //         classNameName="flex items-center gap-2 hover:underline hover:underline-offset-4"
+  //         href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         <Image
+  //           aria-hidden
+  //           src="/window.svg"
+  //           alt="Window icon"
+  //           width={16}
+  //           height={16}
+  //         />
+  //         Examples
+  //       </a>
+  //       <a
+  //         classNameName="flex items-center gap-2 hover:underline hover:underline-offset-4"
+  //         href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         <Image
+  //           aria-hidden
+  //           src="/globe.svg"
+  //           alt="Globe icon"
+  //           width={16}
+  //           height={16}
+  //         />
+  //         Go to nextjs.org →
+  //       </a>
+  //     </footer>
+  //   </div>
+  // );
 }
